b2d30745a5b568ea32df8733669fd425a2ab23fa
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-22 00:30:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
+require 'pry'
+
 # Make your shoe class here!
+
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+  
+  # binding.pry
+  
+  def condition
+    if self.cobble == nil 
+      puts "new"
+    else
+      puts "tattered"
+    end
+  end
+  
+end 
